746ebf41579c09e49efc7988d527473fa22b0a25
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-05-31 10:15:15 +0300

    displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'Just a test',
       'https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'Just a test',
+      'https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg'
+    ),
   ];
 
   constructor() {}
