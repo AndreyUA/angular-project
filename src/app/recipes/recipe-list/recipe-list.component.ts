188668603267472263e596455d8c3d3880e19fb6
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-05-31 09:15:20 +0300

    recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.module';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Array<Recipe> = [
+    new Recipe(
+      'A test recipe',
+      'Just a test',
+      'https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg'
+    ),
+  ];
 
   constructor() {}
 
