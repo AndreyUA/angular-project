d9a4c66233ef0142a9315faa0f776cec89169304
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-06-07 21:02:53 +0300

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../../recipe.module';
 
@@ -11,7 +11,13 @@ export class RecipeItemComponent implements OnInit {
   @Input()
   recipe!: Recipe;
 
+  @Output() recipeSelected = new EventEmitter<void>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.module';
 
@@ -8,15 +8,16 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Array<Recipe> = [
     new Recipe(
-      'A test recipe',
-      'Just a test',
+      'A test recipe 1',
+      'Just a test 1',
       'https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg'
     ),
     new Recipe(
-      'A test recipe',
-      'Just a test',
+      'A test recipe 2',
+      'Just a test 2',
       'https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg'
     ),
   ];
@@ -24,4 +25,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
