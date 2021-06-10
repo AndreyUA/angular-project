9230329028aac7af3481151617fadf5bf769852b
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-06-10 09:23:00 +0300

    improved dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,12 +1,33 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
+// Toggle when click ONLY on element
+// @Directive({
+//   selector: '[appDropdown]',
+// })
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen: boolean = false;
+
+//   @HostListener('click') toggleOpen() {
+//     this.isOpen = !this.isOpen;
+//   }
+// }
+
+// Open if click on element, close if click on element or on document
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen: boolean = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+
+  constructor(private elRef: ElementRef) {}
 }
