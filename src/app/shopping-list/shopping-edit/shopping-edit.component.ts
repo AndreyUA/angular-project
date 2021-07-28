89b01997de3f1ad8c3a8088fd60604104cf58a01
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-07-28 21:13:56 +0300

    6. Updating existing Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -16,7 +16,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   subscription!: Subscription;
   editMode: boolean = false;
-  editedItemIndex: number | null = null;
+  editedItemIndex!: number;
   editedItem!: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) {}
@@ -39,7 +39,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
 
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(
+        this.editedItemIndex,
+        newIngredient
+      );
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
