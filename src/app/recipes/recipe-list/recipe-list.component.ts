c482e9dd27bae5b4123a9d6cbbf9b4ba4f00089a
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-08-01 21:52:33 +0300

    22. Deleting Ingredients and Some Finishing Touches

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
@@ -13,7 +13,7 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Array<Recipe> = [];
   private subscription!: Subscription;
 
@@ -34,5 +34,7 @@ export class RecipeListComponent implements OnInit {
     this.router.navigate(['new'], { relativeTo: this.route });
   }
 
-  // TODO: add unsubscribe!
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
